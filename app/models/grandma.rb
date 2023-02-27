7edd7952eb5da896a6ed87b3aaeacab369e02dbb
Caio Silva <caiotbr@gmail.com>
2023-02-27 17:12:09 +0100

    commit

--- app/models/grandma.rb
+++ app/models/grandma.rb
@@ -1,3 +1,10 @@
 class Grandma < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_id, presence: true
+  has_many :users, through: :bookings, dependent: :destroy
+  has_one_attached :photo
+  validates :description, length: { minimum: 6 }
 end
