22e55718922b0f088f4162fb9b86d14d607a66e7
martin siegert <oddnutshell@aol.com>
2023-03-01 15:53:37 +0100

    new_grandma_form

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,41 +18,39 @@ Grandma.destroy_all
   )
 end
 # Get a list of images in the folder
-folder_name = 'Profile'
-images = Cloudinary::Api.resources(type: 'upload', prefix: "#{folder_name}/")['resources']
-used_images = []
-# Create the grandmas and assign a random image from the folder
-# Get a list of images for the feed photos
-feed_folder_name = 'feed'
-feed_images = Cloudinary::Api.resources(type: 'upload', prefix: "#{feed_folder_name}/")['resources']
-used_feed_images = []
+# folder_name = 'Profile'
+# images = Cloudinary::Api.resources(type: 'upload', prefix: "#{folder_name}/")['resources']
+# used_images = []
+# # Create the grandmas and assign a random image from the folder
+# # Get a list of images for the feed photos
+# feed_folder_name = 'feed'
+# feed_images = Cloudinary::Api.resources(type: 'upload', prefix: "#{feed_folder_name}/")['resources']
+# used_feed_images = []
 # Create the grandmas and assign a random image from the folder and random feed photos
 10.times do
   # Select a random image for the grandma
-  image = images.sample['url']
-  while used_images.include?(image)
-    image = images.sample['url']
-  end
-  used_images << image
-  # Select three random feed photos for the grandma
-  feed_photos = []
-  feed_images = Cloudinary::Api.resources(type: 'upload', prefix: "#{feed_folder_name}/")['resources']
-  used_feed_photos = []
-  while feed_photos.length < 3
-    feed_image = feed_images.sample['url']
-    if !used_feed_photos.include?(feed_image)
-      used_feed_photos << feed_image
-      feed_photos << feed_image
-    end
-  end
+  # image = images.sample['url']
+  # while used_images.include?(image)
+  #   image = images.sample['url']
+  # end
+  # used_images << image
+  # # Select three random feed photos for the grandma
+  # feed_photos = []
+  # feed_images = Cloudinary::Api.resources(type: 'upload', prefix: "#{feed_folder_name}/")['resources']
+  # used_feed_photos = []
+  # while feed_photos.length < 3
+  #   feed_image = feed_images.sample['url']
+  #   if !used_feed_photos.include?(feed_image)
+  #     used_feed_photos << feed_image
+  #     feed_photos << feed_image
+  #   end
+  # end
   # Create the grandma with the selected image and feed photos
   grandma = Grandma.create!(
     first_name: Faker::Name.female_first_name,
     last_name: Faker::Name.last_name,
     location: Faker::Address.full_address,
     skills: Faker::Hobby.activity,
-    image_url: image,
-    feed_photos: feed_photos,
     user_id: User.all.sample.id
   )
 end
