90ff29f4a16a193f609b5f69aa52e271f178f372
Caio Silva <caiotbr@gmail.com>
2023-03-02 11:46:30 +0100

    SEDD Fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 require 'faker'
-require 'cloudinary'
-
 puts 'Getting those amazing grandmas...'
 puts 'Hold on...'
 
@@ -15,7 +13,6 @@ end
 # Create the grandmas with random image URLs and feed photos
 10.times do
   # Generate a random image URL for the grandma
-
   # Create the grandma with the random image and feed ph1oto URLs
   grandma = Grandma.create!(
     first_name: Faker::Name.female_first_name,
@@ -24,4 +21,16 @@ end
     skills: Faker::Hobby.activity,
     user_id: User.all.sample.id
   )
+  # PROFILE PIC
+  rand_num = rand(2..8)
+  file = URI.open("app/assets/images/profile/#{rand_num}- FEED.png")
+    grandma.image_url.attach(io: file, filename: "#{rand_num}.png", content_type: "image/png")
+
+  # FEED
+
+    2.times do
+      rand_num = rand(2..8)
+      file = URI.open("app/assets/images/feed/#{rand_num}- FEED.jpg")
+      grandma.feed_photos.attach(io: file, filename: "#{rand_num}.jpg", content_type: "image/jpg")
+    end
 end
